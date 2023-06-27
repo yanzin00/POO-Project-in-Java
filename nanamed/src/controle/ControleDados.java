e5e5c02939dcaf685fcabf405e085f859842dc34
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-06-27 01:40:48 -0300

    Update ControleDados.java

--- nanamed/src/controle/ControleDados.java
+++ nanamed/src/controle/ControleDados.java
@@ -3,22 +3,34 @@ import modelo.*;
 import java.util.*;
 
 public class ControleDados {
-	private Dados d;
+	private Dados d = new Dados();
 	private Empresa empresa = new Empresa();
 	private Filial filial = new Filial();
 	private ArrayList<Filial> filiais = new ArrayList<Filial>();
+	private int pos;
+	
 	
 	public ControleDados() {
-		d = new Dados();
+		d.fillWithSomeData();
+		filiais = d.getEmpresa().getFiliais();
+		}
+	public void cadastrarEditarFilial(String nome, String cidade, String telefone) {
+		
+		Filial filialTemp = new Filial(nome, cidade,telefone);
+		
+		if(pos == d.getEmpresa().getFiliais().size()) { 
+			d.getEmpresa().setFiliais(filialTemp);
+			filiais = d.getEmpresa().getFiliais();
+			
+		} else {
+	
+			d.getEmpresa().getFiliais().get(pos).setNome(nome);
+			d.getEmpresa().getFiliais().get(pos).setCidade(cidade);
+			d.getEmpresa().getFiliais().get(pos).setTelefone(telefone);
+	    }
 	}
 	
-	public Dados getDados() {
-		return d;
-	}
-	public void setDados(Dados d) {
-		this.d = d;
-	}
-	
+	/*
 	public void cadastrarEditarFilial(String nome, String endereco, String tel, int pos) {
 		Filial filialTemporaria = new Filial(nome, endereco,tel);
 		if (pos == empresa.getFiliais().size()) {
@@ -30,6 +42,7 @@ public class ControleDados {
 		}
 
 	}
+	*/
 	public void setEscritorio(Empresa empresa) {
 		this.empresa = empresa;
 	}
@@ -40,6 +53,34 @@ public class ControleDados {
 
 		if (pos >= 0 && pos < filiais.size()) {
 			filiais.remove(pos);
+			d.getEmpresa().setFiliais(filiais);
 		}
 	}
+	public String[] arrayNomes(String f) {
+		String[] nomes = new String[d.getEmpresa().getFiliais().size()];
+		int x = 0;
+		for (Filial i : d.getEmpresa().getFiliais()) {
+			nomes[x] = i.getNome();
+			x++;
+		}
+		return nomes;
+	}
+	
+	public Dados getD() {
+		return d;
+	}
+	public void setD(Dados d) {
+		this.d = d;
+	}
+	public ArrayList<Filial> getFiliais() {
+		return filiais;
+	}
+	public void setFiliais(ArrayList<Filial> filiais) {
+		this.filiais = filiais;
+		d.getEmpresa().setFiliais(filiais);
+	}
+	
+	
+	
+	
 }
