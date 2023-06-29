4dc167cb3009f0406d0fb0f219d6785ead499638
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-06-29 20:17:12 -0300

    Update ControleDados.java

--- nanamed/src/controle/ControleDados.java
+++ nanamed/src/controle/ControleDados.java
@@ -4,15 +4,21 @@ import java.util.*;
 
 public class ControleDados {
 	private Dados d = new Dados();
-	private Empresa empresa = new Empresa();
-	private Filial filial = new Filial();
 	private ArrayList<Filial> filiais = new ArrayList<Filial>();
+	private ArrayList<Medicamento> medicamentos = new ArrayList<Medicamento>();
+	private ArrayList<Cosmetico> cosmeticos = new ArrayList<Cosmetico>();
+	
+
 	private int pos;
 	
 	
 	public ControleDados() {
 		d.fillWithSomeData();
 		filiais = d.getEmpresa().getFiliais();
+		medicamentos = d.getEmpresa().getFiliais().get(pos).getMedicamento();
+		cosmeticos = d.getEmpresa().getFiliais().get(pos).getCosmetico();
+		ArrayList<Cosmetico> cosmeticos = d.getEmpresa().getFiliais().get(pos).getCosmetico();
+
 		}
 	public void cadastrarEditarFilial(String nome, String cidade, String telefone,int pos) {
 		
@@ -29,29 +35,61 @@ public class ControleDados {
 			d.getEmpresa().getFiliais().get(pos).setTelefone(telefone);
 	    }
 	}	
+	public void cadastrarEditarMedicamento(String nome, int quantidade, double preco, String validade, String codigoDeBarra, String dosagem, String tipo, int index) {
+		
+		Medicamento medicamentoTemp = new Medicamento(nome, quantidade,preco, codigoDeBarra, tipo, validade, dosagem);
+		ArrayList<Medicamento> medicamentos = d.getEmpresa().getFiliais().get(pos).getMedicamento();
+		
+		if(pos == medicamentos.size()) { 
+			 d.getEmpresa().getFiliais().get(pos).setMedicamento(medicamentoTemp);
+			medicamentos = d.getEmpresa().getFiliais().get(pos).getMedicamento();
+			
+		} else {
 	
+			medicamentos.get(pos).setNome(nome);
+			medicamentos.get(pos).setQuantidade(quantidade);
+			medicamentos.get(pos).setPreco(preco);
+			medicamentos.get(pos).setCodigoDeBarra(codigoDeBarra);
+			medicamentos.get(pos).setTipo(tipo);
+			medicamentos.get(pos).setValidade(validade);	
+			medicamentos.get(pos).setDosagem(dosagem);
+	    }
+	}	
+	
+	public void cadastrarEditarCosmetico(String nome, int quantidade, double preco, String validade, String codigoDeBarra, String volume, String marca, int index) {
+		
+		Cosmetico cosmeticoTemp = new Cosmetico(nome,quantidade,preco,validade,codigoDeBarra,volume,marca);
+		ArrayList<Cosmetico> cosmeticos = d.getEmpresa().getFiliais().get(pos).getCosmetico();
+		
+		if(pos == cosmeticos.size()) { 
+			 d.getEmpresa().getFiliais().get(pos).setCosmetico(cosmeticoTemp);
+			cosmeticos = d.getEmpresa().getFiliais().get(pos).getCosmetico();
+			
+		} else {
+	
+			cosmeticos.get(pos).setNome(nome);
+			cosmeticos.get(pos).setQuantidade(quantidade);
+			cosmeticos.get(pos).setPreco(preco);
+			cosmeticos.get(pos).setValidade(validade);	
+			cosmeticos.get(pos).setCodigoDeBarra(codigoDeBarra);
+			cosmeticos.get(pos).setVolume(volume);
+			cosmeticos.get(pos).setMarca(marca);
+			
+	    }
+	}	
+
 	
 	public void setEscritorio(Empresa empresa) {
-		this.empresa = empresa;
 	}
 	
 	
 	public void excluirFilial(int pos) {
-		filiais = empresa.getFiliais();
+		filiais = d.getEmpresa().getFiliais();
 
 		if (pos >= 0 && pos < filiais.size()) {
 			filiais.remove(pos);
 			d.getEmpresa().setFiliais(filiais);
 		}
-	}
-	public String[] arrayNomes(String f) {
-		String[] nomes = new String[d.getEmpresa().getFiliais().size()];
-		int x = 0;
-		for (Filial i : d.getEmpresa().getFiliais()) {
-			nomes[x] = i.getNome();
-			x++;
-		}
-		return nomes;
 	}
 
 	public Dados getD() {
@@ -68,7 +106,13 @@ public class ControleDados {
 		d.getEmpresa().setFiliais(filiais);
 	}
 	
-	
+	public int qtdProd(Dados d){
+		int qtdProdutos = 0;
+		 for (int i = 0; i < d.getEmpresa().getFiliais().get(qtdProdutos).getMedicamento().size() - 1;i++) {
+            qtdProdutos +=d.getEmpresa().getFiliais().get(qtdProdutos).getMedicamento().size() + d.getEmpresa().getFiliais().get(qtdProdutos).getCosmetico().size();
+        }
+	return qtdProdutos;
+	}
 	
 	
 }
