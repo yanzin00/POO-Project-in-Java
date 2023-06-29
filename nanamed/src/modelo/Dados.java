4c296f8120a5e811b2c709e9817bdbac3e99d6e4
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-06-29 20:16:24 -0300

    Update Dados.java

--- nanamed/src/modelo/Dados.java
+++ nanamed/src/modelo/Dados.java
@@ -10,20 +10,23 @@ public class Dados {
 		private ArrayList<Cosmetico> cosmetico = new ArrayList<Cosmetico>();
 		int qtdFiliais = 0;
 		
-		 public void fillWithSomeData() {
-		        for (int i = 0; i < 5; i++) {
+		public void fillWithSomeData() {
+		        for (int i = 0; i <= 5; i++) {
 		        	Filial fil = new Filial("Filial"+i,"cidade"+i,""+(i+1)*1000000);
-		        		
-		        	Medicamento med = new Medicamento("medicamento"+i*2,(i+1)*420,(i+2)*2.99,"10/10/202"+(i+2),"12321312"+(i*2)*10,(i*50)+"mg","tipo"+i);
-		        		
-		        	Cosmetico cos = new Cosmetico("medicamento"+i*2,(i+1)*420,(i+2)*2.99,"23/03/202"+(i+2),"12321312"+(i*2)*10,(i*50)+"mL","marca"+i);
-		        	fil.getCosmetico().add(cos);
-		        	fil.getMedicamento().add(med);
-		        	
+					fillCosmeticAndMeds(fil);
 		        	empresa.setFiliais(fil);
 		        }
-		      
-		      }
+		}
+
+		public void fillCosmeticAndMeds(Filial fil) {
+			for (int j = 0; j <= 5; j++){
+				Medicamento med = new Medicamento("med"+j,(j+1)*420,(j+2)*1.99,"10/10/202"+(j+2),"101101"+(j*2)*10,(j*50)+"mg","tipo"+j);
+				Cosmetico cos = new Cosmetico("cos"+j,(j+1)*420,(j+2)*1.99,"23/03/202"+(j+2),"10010"+(j*2)*10,(j*50)+"mL","marca"+j);
+
+				fil.getCosmetico().add(cos);
+				fil.getMedicamento().add(med);
+			}
+		}
 		 	
 		public void cadastroFilial(Filial f) {
 				filiais.add(f);
