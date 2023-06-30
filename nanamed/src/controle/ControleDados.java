fedac70b8268b524a7079343b911a67c317e39fb
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-06-30 18:09:44 -0300

    Update ControleDados.java

--- nanamed/src/controle/ControleDados.java
+++ nanamed/src/controle/ControleDados.java
@@ -1,96 +1,102 @@
 package controle;
+
 import modelo.*;
 import java.util.*;
 
 public class ControleDados {
+
 	private Dados d = new Dados();
 	private ArrayList<Filial> filiais = new ArrayList<Filial>();
 	private ArrayList<Medicamento> medicamentos = new ArrayList<Medicamento>();
 	private ArrayList<Cosmetico> cosmeticos = new ArrayList<Cosmetico>();
-	
-
 	private int pos;
-	
+	private int index;
 	
 	public ControleDados() {
 		d.fillWithSomeData();
 		filiais = d.getEmpresa().getFiliais();
 		medicamentos = d.getEmpresa().getFiliais().get(pos).getMedicamento();
 		cosmeticos = d.getEmpresa().getFiliais().get(pos).getCosmetico();
-		ArrayList<Cosmetico> cosmeticos = d.getEmpresa().getFiliais().get(pos).getCosmetico();
-
+		 
 		}
 	public void cadastrarEditarFilial(String nome, String cidade, String telefone,int pos) {
-		
 		Filial filialTemp = new Filial(nome, cidade,telefone);
 		
-		if(pos == d.getEmpresa().getFiliais().size()) { 
+			if(index == d.getEmpresa().getFiliais().size()) { 
 			d.getEmpresa().setFiliais(filialTemp);
 			filiais = d.getEmpresa().getFiliais();
 			
-		} else {
+			} 
+			else {
 	
 			d.getEmpresa().getFiliais().get(pos).setNome(nome);
 			d.getEmpresa().getFiliais().get(pos).setCidade(cidade);
 			d.getEmpresa().getFiliais().get(pos).setTelefone(telefone);
-	    }
+	    	}
 	}	
-	public void cadastrarEditarMedicamento(String nome, int quantidade, double preco, String validade, String codigoDeBarra, String dosagem, String tipo, int index) {
-		
+	public void cadastrarEditarMedicamento(String nome, int quantidade, double preco, String validade, String codigoDeBarra, String dosagem, String tipo, int index, int pos){
 		Medicamento medicamentoTemp = new Medicamento(nome, quantidade,preco, codigoDeBarra, tipo, validade, dosagem);
-		ArrayList<Medicamento> medicamentos = d.getEmpresa().getFiliais().get(pos).getMedicamento();
 		
-		if(pos == medicamentos.size()) { 
-			 d.getEmpresa().getFiliais().get(pos).setMedicamento(medicamentoTemp);
-			medicamentos = d.getEmpresa().getFiliais().get(pos).getMedicamento();
+		
+			if(index == medicamentos.size() ) { 
+				d.getEmpresa().getFiliais().get(pos).setMedicamento(medicamentoTemp);
+				medicamentos = d.getEmpresa().getFiliais().get(pos).getMedicamento();
 			
-		} else {
+			} 
+			else {
 	
-			medicamentos.get(pos).setNome(nome);
-			medicamentos.get(pos).setQuantidade(quantidade);
-			medicamentos.get(pos).setPreco(preco);
-			medicamentos.get(pos).setCodigoDeBarra(codigoDeBarra);
-			medicamentos.get(pos).setTipo(tipo);
-			medicamentos.get(pos).setValidade(validade);	
-			medicamentos.get(pos).setDosagem(dosagem);
-	    }
+				medicamentos.get(pos).setNome(nome);
+				medicamentos.get(pos).setQuantidade(quantidade);
+				medicamentos.get(pos).setPreco(preco);
+				medicamentos.get(pos).setCodigoDeBarra(codigoDeBarra);
+				medicamentos.get(pos).setTipo(tipo);
+				medicamentos.get(pos).setValidade(validade);	
+				medicamentos.get(pos).setDosagem(dosagem);
+	    	}
 	}	
 	
 	public void cadastrarEditarCosmetico(String nome, int quantidade, double preco, String validade, String codigoDeBarra, String volume, String marca, int index) {
+		//Cosmetico cosmeticoTemp = new Cosmetico(nome,quantidade,preco,validade,codigoDeBarra,volume,marca);
 		
-		Cosmetico cosmeticoTemp = new Cosmetico(nome,quantidade,preco,validade,codigoDeBarra,volume,marca);
-		ArrayList<Cosmetico> cosmeticos = d.getEmpresa().getFiliais().get(pos).getCosmetico();
-		
-		if(pos == cosmeticos.size()) { 
-			 d.getEmpresa().getFiliais().get(pos).setCosmetico(cosmeticoTemp);
-			cosmeticos = d.getEmpresa().getFiliais().get(pos).getCosmetico();
+		/*
+			if(index == cosmeticos.size()) { 
+				d.getEmpresa().getFiliais().get(pos).setCosmetico(cosmeticoTemp);
+				cosmeticos = d.getEmpresa().getFiliais().get(pos).getCosmetico();
 			
-		} else {
-	
-			cosmeticos.get(pos).setNome(nome);
-			cosmeticos.get(pos).setQuantidade(quantidade);
-			cosmeticos.get(pos).setPreco(preco);
-			cosmeticos.get(pos).setValidade(validade);	
-			cosmeticos.get(pos).setCodigoDeBarra(codigoDeBarra);
-			cosmeticos.get(pos).setVolume(volume);
-			cosmeticos.get(pos).setMarca(marca);
-			
-	    }
-	}	
-
-	
-	public void setEscritorio(Empresa empresa) {
-	}
-	
+			} 
+			else {
+	*/
+				cosmeticos.get(index).setNome(nome);
+				cosmeticos.get(index).setQuantidade(quantidade);
+				cosmeticos.get(index).setPreco(preco);
+				cosmeticos.get(index).setValidade(validade);	
+				cosmeticos.get(index).setCodigoDeBarra(codigoDeBarra);
+				cosmeticos.get(index).setVolume(volume);
+				cosmeticos.get(index).setMarca(marca);
+				System.out.println("--------------------get pos vvvv--");
+				System.out.println(cosmeticos.get(pos));
+				System.out.println(cosmeticos.get(index)+" get index");
+	    	}
+	//}	
 	
 	public void excluirFilial(int pos) {
 		filiais = d.getEmpresa().getFiliais();
 
-		if (pos >= 0 && pos < filiais.size()) {
-			filiais.remove(pos);
-			d.getEmpresa().setFiliais(filiais);
-		}
+			if (pos >= 0 && pos < filiais.size()) {
+				filiais.remove(pos);
+				d.getEmpresa().setFiliais(filiais);
+			}
 	}
+	
+	public int qtdProd(Dados d){
+		int qtdProdutos = 0;
+			for (int i = 0; i < d.getEmpresa().getFiliais().get(qtdProdutos).getMedicamento().size() - 1;i++) {
+				qtdProdutos +=d.getEmpresa().getFiliais().get(qtdProdutos).getMedicamento().size() + d.getEmpresa().getFiliais().get(qtdProdutos).getCosmetico().size();
+			}
+	return qtdProdutos;
+	}
+
+	//gets e sets
 
 	public Dados getD() {
 		return d;
@@ -101,17 +107,16 @@ public class ControleDados {
 	public ArrayList<Filial> getFiliais() {
 		return filiais;
 	}
+	
+	public Filial getFilial(int index) {
+		return d.getEmpresa().getFiliais().get(index);
+	}
+	public void setFilial(ArrayList<Filial> filial){
+			d.getEmpresa().setFiliais(filial);
+	}
 	public void setFiliais(ArrayList<Filial> filiais) {
 		this.filiais = filiais;
 		d.getEmpresa().setFiliais(filiais);
-	}
-	
-	public int qtdProd(Dados d){
-		int qtdProdutos = 0;
-		 for (int i = 0; i < d.getEmpresa().getFiliais().get(qtdProdutos).getMedicamento().size() - 1;i++) {
-            qtdProdutos +=d.getEmpresa().getFiliais().get(qtdProdutos).getMedicamento().size() + d.getEmpresa().getFiliais().get(qtdProdutos).getCosmetico().size();
-        }
-	return qtdProdutos;
 	}
 	
 	
