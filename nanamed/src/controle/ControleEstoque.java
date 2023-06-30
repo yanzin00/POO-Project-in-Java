8f451b72e0254cf560698e5dad9b8120ad70634a
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-06-30 18:10:07 -0300

    Update ControleEstoque.java

--- nanamed/src/controle/ControleEstoque.java
+++ nanamed/src/controle/ControleEstoque.java
@@ -3,37 +3,27 @@ package controle;
 import java.util.*;
 import modelo.*;
 
-
 public class ControleEstoque {
-
-
-
 
 	public ControleDados dados = new ControleDados();	
 	private ArrayList<Medicamento> medicamentos = new ArrayList<Medicamento>();
 	private ArrayList<Cosmetico> cosmeticos = new ArrayList<Cosmetico>();
 	
-	
+
+	public ControleEstoque(ControleDados d) {
+
+		for (int i = 0; i < d.getD().getEmpresa().getFiliais().size() ; i++) {
+			for (int j = 0; j < d.getD().getEmpresa().getFiliais().get(i).getCosmetico().size() ; j++) {
+				cosmeticos.add(d.getD().getEmpresa().getFiliais().get(i).getCosmetico().get(j));
+			}
+
+		}
+	}
+
 	
 	public ControleEstoque(ControleDados d, int pos) {
 		medicamentos = d.getD().getEmpresa().getFiliais().get(pos).getMedicamento();
 		cosmeticos = d.getD().getEmpresa().getFiliais().get(pos).getCosmetico();
-	}
-	public ArrayList<Cosmetico> getCosmeticos() {
-		return cosmeticos;
-	}
-
-	public void setCosmeticos(ArrayList<Cosmetico> cosmeticos) {
-		this.cosmeticos = cosmeticos;
-	}
-
-
-	public ArrayList<Medicamento> getMedicamento() {
-		return medicamentos;
-	}
-
-	public int getQtdMedicamento() {
-		return medicamentos.size();
 	}
 
 	public void setMedicamento(ArrayList<Medicamento> medicamentos) {
@@ -66,11 +56,32 @@ public class ControleEstoque {
 			estoque[i+5] = cosmeticos.get(i).toString();
 		}
 
+		
+
 		System.out.println("--------------------------");
 		System.out.println(meds.length);
 		System.out.println(cosm.length);
 		
 		return estoque;
+	}
+
+	// gets e sets
+
+	public ArrayList<Cosmetico> getCosmeticos() {
+		return cosmeticos;
+	}
+
+	public void setCosmeticos(ArrayList<Cosmetico> cosmeticos) {
+		this.cosmeticos = cosmeticos;
+	}
+
+
+	public ArrayList<Medicamento> getMedicamento() {
+		return medicamentos;
+	}
+
+	public int getQtdMedicamento() {
+		return medicamentos.size();
 	}
 
 }
