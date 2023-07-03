7b477d464f14be52225c866d3522759c07e51512
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-07-02 23:12:45 -0300

    Update ControleFilial.java

--- nanamed/src/controle/ControleFilial.java
+++ nanamed/src/controle/ControleFilial.java
@@ -3,39 +3,44 @@ package controle;
 import java.util.ArrayList;
 import modelo.*;
 
-public class ControleFilial  {
+/**
+	 * Classe que faz a ponte entre view e model, com métodos feitos em relação as filiais.
+   * 
+	 * @author Yan Lucas, Felipe Matheus
+	 * @since 2023
+	 * @version 1.0
+	 */
 
-	Empresa empresa = new Empresa();
-	public ArrayList<Filial> filiais = new ArrayList<Filial>();
-	
+public class ControleFilial {
 
-	public ControleFilial(ControleDados cd) {
-		filiais = cd.getD().getEmpresa().getFiliais();
-		
-	}
+  Empresa empresa = new Empresa();
+  public ArrayList<Filial> filiais = new ArrayList<Filial>();
 
-	public ArrayList<Filial> getFiliais() {
-		return filiais;
-	}
+  public ControleFilial(ControleDados cd) {
+    filiais = cd.getD().getEmpresa().getFiliais();
+  }
 
+  public ArrayList<Filial> getFiliais() {
+    return filiais;
+  }
 
-	public void setFiliais(ArrayList<Filial> filiais) {
-		this.filiais = filiais;
-	}
-	
-	public int getQtF() {
-		return filiais.size();
-	}
-	
-	public String[] getNomesFiliais() {
-		String[] nomes = new String[filiais.size()];
-		
-		for (int i = 0; i < filiais.size(); i++) {
-			nomes[i] = filiais.get(i).getNome();
-		}
-		
-		return nomes;
-	}
+  public void setFiliais(ArrayList<Filial> filiais) {
+    this.filiais = filiais;
+  }
+
+  public int getQtF() {
+    return filiais.size();
+  }
+
+  // O método getNomesFiliais() retorna uma array com os nomes das filiais.
+
+  public String[] getNomesFiliais() {
+    String[] nomes = new String[filiais.size()];
+
+    for (int i = 0; i < filiais.size(); i++) {
+      nomes[i] = filiais.get(i).getNome();
+    }
+
+    return nomes;
+  }
 }
-
-
