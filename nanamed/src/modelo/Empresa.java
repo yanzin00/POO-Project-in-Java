a7fe9fd05ff2789ec08adff384940b86713789d2
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-07-02 23:13:28 -0300

    Update Empresa.java

--- nanamed/src/modelo/Empresa.java
+++ nanamed/src/modelo/Empresa.java
@@ -2,39 +2,37 @@ package modelo;
 
 import java.util.ArrayList;
 
+/**
+ * Classe Empresa onde se tem o CRUD empresa e suas informações
+ * @author Yan Lucas, Felipe Matheus
+ * @since 2023
+ * @version 1.0
+ * @see nomeEmpresa nome da Empresa
+ 
+ */
+
 public class Empresa {
 	
 	private String nomeEmpresa;
 	public ArrayList<Filial> filiais = new ArrayList<Filial>();
 
-	public void setFiliais(ArrayList<Filial> filiais) {
-		this.filiais = filiais;
-	}
+	/**
+     * Método toString() da classe Empresa. Constroi uma String contendo o valor de todas as váriaveis da classe Empresa.
+     * @return String
+     */
 	
-	public  ArrayList<Filial> getFiliais() {
-		return filiais;
+	public String toString() {
+		return nomeEmpresa + filiais;
 	}
-	
-	public String getNomeEmpresa() {
-		return nomeEmpresa;
-	}
-	public void setNomeEmpresa(String nomeEmpresa) {
-		this.nomeEmpresa = nomeEmpresa;
-	}
-
-	public void setFiliais(Filial filialTemporaria) {
-		filiais.add(filialTemporaria);
-		
-	}
-	public Empresa( String nomeEmpresa, ArrayList<Filial> filiais) {
-		
-		this.nomeEmpresa = nomeEmpresa;
-		this.filiais = filiais;
-		}
-	
+	// construtor para poder chamar nas outras classes
 	public Empresa() {
     }
 	
+	/**
+	 * Método para editar e cadastrar as filiais 
+	 * @param pos 		posição da filial na ArrayList
+	 * @param filial 		parametro usado para se editar e cadastrar 
+	 */
     public void editarCadastrarFilial(int pos, Filial filial) {
 		if (pos == filiais.size()) {
 			filiais.add(filial);
@@ -46,26 +44,41 @@ public class Empresa {
 			filiais.get(pos).setMedicamento(filial.getMedicamento());
 		}
 	}
-	// achamos que nao ta sendo utilizado
-
-	public String[] listarFiliais() {
-		String[] filiaisListadas = new String[filiais.size()];
-		for(int i = 0; i < filiais.size() - 1; i++) {
-			filiaisListadas[i] = filiais.get(i).getNome();
-		}
-		return filiaisListadas;
-	}
 	
+	//cadastrar a filial nova
 	public void cadastrarFilial(Filial filial) {
 		filiais.add(filial);
 		
 	}
 	
+	//remover filial registrada
 	public void removerFilial(Filial filial) {
 		filiais.remove(filial);
 	}
 
-	public String toString() {
-		return nomeEmpresa + filiais;
-	}	
+	//get e set da filiais
+	public void setFiliais(ArrayList<Filial> filiais) {
+		this.filiais = filiais;
+	}
+	
+	public  ArrayList<Filial> getFiliais() {
+		return filiais;
+	}
+	
+	//get e set do nome da empresa
+	public String getNomeEmpresa() {
+		return nomeEmpresa;
+	}
+	public void setNomeEmpresa(String nomeEmpresa) {
+		this.nomeEmpresa = nomeEmpresa;
+	}
+
+	// set da filial temporaria
+	public void setFiliais(Filial filialTemporaria) {
+		filiais.add(filialTemporaria);
+		
+	}
+
+	
+		
 }
