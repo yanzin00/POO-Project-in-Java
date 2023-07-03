c2ad0170448f5b129f5738b290bd274350e015b2
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-07-02 23:13:41 -0300

    Update Filial.java

--- nanamed/src/modelo/Filial.java
+++ nanamed/src/modelo/Filial.java
@@ -1,6 +1,16 @@
 package modelo;
 
 import java.util.*;
+/**
+ * Classse que cria as filiais
+ * @author Yan Lucas, Felipe Matheus
+ * @since 2023
+ * @version 1.0	
+ * @see nome 		nome da filial
+ * @see cidade 		cidade onde fica a filial
+ * @see telefone 		nomero do telefone da filial
+ * 
+ */
 
 public class Filial {
 
@@ -9,41 +19,51 @@ public class Filial {
 	private String telefone;
 	private ArrayList<Medicamento> medicamento = new ArrayList<Medicamento>();
 	private ArrayList<Cosmetico> cosmetico = new ArrayList<Cosmetico>();
+
+	/**
+	 * Método que se cria as filiais 
+	 * @param nome 		nome das filiais
+	 * @param cidade 		cidade da filial
+	 * @param telefone		telefone da filial
+	 */
 	
 	public Filial(String nome, String cidade,String telefone) {
 		this.nome = nome;
 		this.cidade = cidade;
 		this.telefone = telefone;
 	}
-
+	//construto utilizado na classe Dados
 	public Filial() {
 
 	}
 
+	@Override
 	public String toString() {
 		return nome + " " + cidade +" "+ telefone;
 	}
 
-	// gets e sets
-
+	//get e set do Nome
 	public String getNome() {
 		return nome;
 	}	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	//get e set da Cidade
 	public String getCidade() {
 		return cidade;
 	}
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
+	// get e set do Telefone
 	public String getTelefone() {
 		return telefone;
 	}
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+	//get e set da arraylist Medicamento
 	public ArrayList<Medicamento> getMedicamento() {
 		return medicamento;
 	}
@@ -52,18 +72,18 @@ public class Filial {
 		this.medicamento = medicamento;
 	}
 
+	// get e set da arraylist Cosmetico
 	public ArrayList<Cosmetico> getCosmetico() {
 		return cosmetico;
 	}
-
 	public void setCosmetico(ArrayList<Cosmetico> cosmetico) {
 		this.cosmetico = cosmetico;
 	}
-
+	//set do medicamentoTemp
     public void setMedicamento(Medicamento medicamentoTemp) {
 		medicamento.add(medicamentoTemp);
     }
-
+	//set do cosmeticoTemp
     public void setCosmetico(Cosmetico cosmeticoTemp) {
 		cosmetico.add(cosmeticoTemp);
     }
